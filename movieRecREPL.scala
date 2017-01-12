340ac6dd6e2468b4dd1c629a8d37bbc6057d463a
markostam <markostam@gmail.com>
2017-01-12 18:49:34 -0500

    first pass CF done with MSE

--- movieRecREPL.scala
+++ movieRecREPL.scala
@@ -38,7 +38,26 @@ val numTest = test.count()
 println("Training: " + numTraining + ", validation: " 
   + numValidation + ", test: " + numTest)
 
+val rank = 10
+val numIterations = 10
+val lambda = 0.01
+val model = ALS.train(training, rank, numIterations, lambda)
 
+val usersProducts = validation.map { case Rating(user, product, rate) =>
+  (user, product)
+}
 
+val predictions =
+  model.predict(usersProducts).map { case Rating(user, product, rate) =>
+    ((user, product), rate)
+  }
 
+val ratesAndPreds = validation.map { case Rating(user, product, rate) =>
+  ((user, product), rate)
+}.join(predictions)
+
+val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+  val err = (r1 - r2)
+  err * err
+}.mean()
 

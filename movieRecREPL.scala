6274c667e6abac802fe430a7cc32da4673b104e4
markostam <markostam@gmail.com>
2017-01-13 16:44:13 -0500

    updated movieRecREPL.scala with grid param search

--- movieRecREPL.scala
+++ movieRecREPL.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.mllib.recommendation.{ALS, Rating, MatrixFactorizationModel}
+import org.apache.spark.rdd._
 
 // import data and split into useful RDD's with Ratin
 val ratingsLoc = "/Users/markostamenovic/code/movierec-spark/data/ml-10M100K/ratings.dat"
@@ -17,7 +18,7 @@ val numMovies = ratings.map(_._2.product).distinct.count
 println("Got " + numRatings + " ratings from "
   + numUsers + " users on " + numMovies + " movies.")
 
-// split training data
+// split into train/test/validation
 
 val numPartitions = 4
 val training = ratings.filter(x => x._1 < 6).
@@ -38,26 +39,55 @@ val numTest = test.count()
 println("Training: " + numTraining + ", validation: " 
   + numValidation + ", test: " + numTest)
 
-val rank = 10
-val numIterations = 10
-val lambda = 0.01
-val model = ALS.train(training, rank, numIterations, lambda)
-
-val usersProducts = validation.map { case Rating(user, product, rate) =>
-  (user, product)
+def computeRmse(model: MatrixFactorizationModel, 
+                ratesAndPreds: RDD[((Int, Int), (Double, Double))]) = {
+  val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
+    val err = (r1 - r2)
+    err * err
+  }.mean()
+  math.sqrt(MSE)
 }
 
-val predictions =
-  model.predict(usersProducts).map { case Rating(user, product, rate) =>
-    ((user, product), rate)
+val ranks = List(3, 10, 30, 100)
+val lambdas = List(0.01, 0.1, 1.0, 10.0)
+val numIters = List(10, 30, 100)
+val bestValidationRmse = Long.MaxValue
+
+for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
+  val model = ALS.train(training, rank, numIter, lambda)
+
+  val usersProducts = validation.map { case Rating(user, product, rate) =>
+    (user, product) }
+  val predictions =
+    model.predict(usersProducts).map { case Rating(user, product, rate) =>
+      ((user, product), rate) }
+  val ratesAndPreds = validation.map { case Rating(user, product, rate) =>
+    ((user, product), rate) }.join(predictions)
+
+  val validationRmse = computeRmse(model, ratesAndPreds)
+  println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
+    + rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".")
+  if (validationRmse < bestValidationRmse) {
+    println("New best RMSE: " + validationRmse)
+    val bestModel = Some(model)
+    val bestValidationRmse = validationRmse
+    val bestRank = rank
+    val bestLambda = lambda
+    val bestNumIter = numIter
   }
+}
 
-val ratesAndPreds = validation.map { case Rating(user, product, rate) =>
-  ((user, product), rate)
-}.join(predictions)
+val myRatedMovieIds = myRatings.map(_.product).toSet
+val candidates = sc.parallelize(movies.keys.filter(!myRatedMovieIds.contains(_)).toSeq)
+val recommendations = bestModel.get.
+  predict(candidates.map((0, _))).
+  collect().
+  sortBy(- _.rating).
+  take(50)
 
-val MSE = ratesAndPreds.map { case ((user, product), (r1, r2)) =>
-  val err = (r1 - r2)
-  err * err
-}.mean()
-
+var i = 1
+println("Movies recommended for you:")
+recommendations.foreach { r =>
+  println("%2d".format(i) + ": " + movies(r.product))
+  i += 1
+}
